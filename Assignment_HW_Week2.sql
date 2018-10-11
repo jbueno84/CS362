bd4206b2ac6a8298f6ced334e720e6c4b9c9d15f
julia.bueno <julia.bueno@DEN.coloradotech.edu>
2018-10-10 19:47:30 -0600

    Assignment_HW_Week2.sql

---
+++ Assignment_HW_Week2.sql
@@ -0,0 +1,14 @@
+/*
+* Description:	This script is created to teach students Structure Query Language for Data Management.
+*
+* Notes: Lecture 2: SELECT & ORDER BY
+**************************************************************************************************
+* Change History
+**************************************************************************************************
+* Date:10/10/2018			Author:		Julia Bueno	Description:
+* ----------    ---------- 		--------------------------------------------------
+* 09/07/2018	jbueno84			initial version created
+************************************************************************************************/
+Select TOP(10) *
+from dbo.Lake
+order by Area desc
